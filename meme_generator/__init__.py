95bb8f3ee35d6cc19fc534f29e0ccffbc1b213b8
haotiaz <Haotian.a.zheng@gmail.com>
2022-04-12 11:10:22 -0400

    Upload basic configuration files and directories

---
+++ meme_generator/__init__.py
@@ -0,0 +1,10 @@
+"""Meme generator package initializer."""
+
+import flask
+# app is a single object used by all the code modules in this package
+app = flask.Flask(__name__)
+# Read settings from config module (meme_generator/config.py)
+app.config.from_object('meme_generator.config')
+
+import meme_generator.views
+import meme_generator.api
---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+Meme Generator python package configuration.
+"""
+
+from setuptools import setup
+
+setup(
+    name='meme_generator',
+    version='0.1.0',
+    packages=['meme_generator'],
+    include_package_data=True,
+    install_requires=[
+        'arrow',
+        'bs4',
+        'Flask',
+        'html5validator',
+        'pycodestyle',
+        'pydocstyle',
+        'pylint',
+        'pytest',
+        'requests',
+        'selenium',
+    ],
+    python_requires='>=3.6',
+)
