7609d73c7bb3ee22a48fd1b5ef91e81965bf0057
haotiaz <Haotian.a.zheng@gmail.com>
2022-04-13 18:43:29 -0400

    feat: implement infinite scroll when showing the search result

--- meme_generator/js/search.jsx
+++ meme_generator/js/search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import ImagePreview from './search_components/ImagePreview';
 
 class Search extends React.Component {
   /* Display the search results
@@ -7,16 +8,76 @@ class Search extends React.Component {
 
   constructor(props) {
     super(props);
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    this.state = {query: params.get('q') || "", 
+                  images: [],
+                  hasMore: true,
+                  currentPage: 0
+                 };
+    this.fetchData = this.fetchData.bind(this);
   }
 
   componentDidMount() {
-    
+    this.fetchData();
+  }
+
+  buildImageUrl(serverId, id, secret, size) {
+    // Return the URL of image
+    return `https://live.staticflickr.com/${serverId}/${id}_${secret}_${size}.jpg`;
+  }
+
+  fetchData() {
+    // Call REST API to add the images to the list
+    this.setState((prevState) => ({
+      currentPage: prevState.currentPage+1
+    }));
+    const { query, currentPage } = this.state;
+    const perPage = 50;
+    let api_url = `/api/v1/search/?q=${query}&per_page=${perPage}&page=${currentPage}`;
+    fetch(api_url, { credentials: 'same-origin' })
+      .then((response) => {
+        if (!response.ok) {
+          this.setState({
+            hasMore: false
+          });
+          throw Error(response.statusText);
+        } 
+        return response.json();
+      }).then((data) => {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...data.photos.photo]
+        }));
+        if(data.photos.photo.length < perPage){
+          this.setState({
+            hasMore: false
+          });
+        }
+
+      }).catch((error) => console.log(error));
   }
 
   render() {
+    let { images, hasMore } = this.state;
+    let imageItems = images.map((image, index) => (
+      <ImagePreview key={index} id={image.id} server={image.server} secret={image.secret} title={image.title}/>
+    ));
+
     return (
       <div>
-        Search
+        <InfiniteScroll
+          dataLength = { imageItems.length }
+          next = { this.fetchData }
+          hasMore = { hasMore }
+          loader = { <h4>Loading...</h4> }
+          endMessage = {
+            <p style = { { textAlign: 'center' } }>
+              <b>You have seen it all</b>
+            </p>
+          }
+        >
+          <div>{ imageItems }</div>
+        </InfiniteScroll>
       </div>
     );
   }
---
+++ meme_generator/js/search_components/ImagePreview.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function buildImageUrl(server, id, secret, size) {
+  // Return the URL of image
+  return `https://live.staticflickr.com/${server}/${id}_${secret}_${size}.jpg`;
+}
+
+function ImagePreview(props) {
+  // Image preview in the search result
+  const { id, server, secret, title } = props;
+  let previewImageUrl = buildImageUrl(server, id, secret, 'n');
+  let largeImageUrl = encodeURIComponent(buildImageUrl(server, id, secret, 'b'));
+
+  return (
+    <Link
+      to={{
+      pathname: "/edit/",
+      search: `?url=${largeImageUrl}`
+      }}
+    >
+      <img src={ previewImageUrl } alt={ title } height='200' />
+    </Link>
+  );
+}
+
+ImagePreview.propTypes = {
+  server: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  secret: PropTypes.string.isRequired,
+  title: PropTypes.string
+};
+
+export default ImagePreview;
