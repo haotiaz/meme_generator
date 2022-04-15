9f8b04edf7863f859b7d651aa95b0fd7332c54e9
haotiaz <Haotian.a.zheng@gmail.com>
2022-04-15 14:59:59 -0400

    build: remove unnecessary python external packages

--- setup.py
+++ setup.py
@@ -10,16 +10,8 @@ setup(
     packages=['meme_generator'],
     include_package_data=True,
     install_requires=[
-        'arrow',
-        'bs4',
         'Flask',
-        'html5validator',
-        'pycodestyle',
-        'pydocstyle',
-        'pylint',
-        'pytest',
         'requests',
-        'selenium',
     ],
     python_requires='>=3.6',
 )
